4678f6b557e7f1f7f94c76843e6f08d5320785cd
Ondrej Certik <ondrej@certik.cz>
2012-05-25 04:02:31 -0700

    Add a plot_paraview.py script

---
+++ plot_paraview.py
@@ -0,0 +1,10 @@
+from paraview.simple import *
+#paraview.simple._DisableFirstRenderCameraReset()
+
+Ra_vtr = XMLRectilinearGridReader(FileName=['/tmp/atomplot/Ra.vtr'])
+Contour1 = Contour(PointMergeMethod="Uniform Binning")
+Contour1.ContourBy = ['POINTS', 'Orbital(n=1,l=0,m=0)']
+Contour1.Isosurfaces = [0.4829863905906677]
+DataRepresentation2 = Show()
+Render()
+WriteImage("orbital_n1l0m0.png")
