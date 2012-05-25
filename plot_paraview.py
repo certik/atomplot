4c4665c129737331d386c63f09eb961d5209bacc
Ondrej Certik <ondrej@certik.cz>
2012-05-25 04:04:04 -0700

    Fix scripts

--- plot_paraview.py
+++ plot_paraview.py
@@ -1,7 +1,7 @@
 from paraview.simple import *
 #paraview.simple._DisableFirstRenderCameraReset()
 
-Ra_vtr = XMLRectilinearGridReader(FileName=['/tmp/atomplot/Ra.vtr'])
+Ra_vtr = XMLRectilinearGridReader(FileName=['Ra.vtr'])
 Contour1 = Contour(PointMergeMethod="Uniform Binning")
 Contour1.ContourBy = ['POINTS', 'Orbital(n=1,l=0,m=0)']
 Contour1.Isosurfaces = [0.4829863905906677]
