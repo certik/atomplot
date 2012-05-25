092df535e82513fe9f477b9796bb4a77c2a87e4d
Ondrej Certik <ondrej@certik.cz>
2012-05-25 04:36:03 -0700

    Process all states

--- plot_paraview.py
+++ plot_paraview.py
@@ -1,9 +1,33 @@
+import sys
+import os
 from paraview.simple import *
 #paraview.simple._DisableFirstRenderCameraReset()
 
+def get_nlm(i):
+    j = 0
+    for l in [0, 1, 2, 3]:
+        if l == 0:
+            nmax = 7
+        elif l == 1:
+            nmax = 6
+        elif l == 2:
+            nmax = 5
+        elif l == 3:
+            nmax = 4
+        for n in range(l+1, nmax+1):
+            for m in range(-l, l+1):
+                if j == i:
+                    return n, l, m
+                j += 1
+
+os.system("mkdir -p img")
+i = int(sys.argv[1])
+n, l, m = get_nlm(i)
+print "Processing:", n, l, m
+
 Ra_vtr = XMLRectilinearGridReader(FileName=['Ra.vtr'])
 Contour1 = Contour(PointMergeMethod="Uniform Binning")
-Contour1.ContourBy = ['POINTS', 'Orbital(n=5,l=2,m=1)']
+Contour1.ContourBy = ['POINTS', 'Orbital(n=%d,l=%d,m=%d)' % (n, l, m)]
 Contour1.Isosurfaces = [0.02]
 DataRepresentation2 = Show()
 
@@ -17,4 +41,4 @@ RenderView1.CameraClippingRange = [6.186487737063765, 14.545743147772642]
 
 
 Render()
-WriteImage("orbital_n1l0m0.png")
+WriteImage("img/orbital_n%dl%dm%d.png" % (n, l, m))
